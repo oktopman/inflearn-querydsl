36a0ad3c138412ac1dfe568fd2f08fdd7a5343d2
oktopman <colaws3@gmail.com>
2020-11-05 01:06:02 +0900

    querydsl 기본문법.  ~집합

--- src/test/java/com/inflearn/querydslstudy/QuerydslBasicTest.java
+++ src/test/java/com/inflearn/querydslstudy/QuerydslBasicTest.java
@@ -3,7 +3,11 @@ package com.inflearn.querydslstudy;
 import com.inflearn.querydslstudy.entity.Member;
 import com.inflearn.querydslstudy.entity.QMember;
 import com.inflearn.querydslstudy.entity.Team;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +15,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
+import static com.inflearn.querydslstudy.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -44,14 +50,104 @@ public class QuerydslBasicTest {
 
     @Test
     void start_query_dsl_test() {
-        QMember qMember = new QMember("m");
-        Member member = queryFactory
-                .select(qMember)
-                .from(qMember)
-                .where(qMember.age.eq(30))
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.age.eq(30))
                 .fetchOne();
 
-        assertThat(member.getTeam().getName()).isEqualTo("teamB");
+        assertThat(findMember.getTeam().getName()).isEqualTo("teamB");
 
     }
+
+    @Test
+    void search_and_param_test() {
+        Member findMember = queryFactory
+                .selectFrom(QMember.member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.between(10, 30)
+                ).fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void result_featch_test() {
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member fetchOne = queryFactory
+                .selectFrom(QMember.member)
+                .where(member.age.eq(10))
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(QMember.member)
+                .fetchFirst();
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        assertThat(results.getTotal()).isEqualTo(4);
+        assertThat(results.getResults().size()).isEqualTo(4);
+    }
+
+    /*
+     * 1. 회원 나이 내림차순
+     * 2. 회원 이름 올림차순
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     * */
+    @Test
+    void sort_test() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(members.get(0).getUsername()).isEqualTo("member5");
+        assertThat(members.get(1).getUsername()).isEqualTo("member6");
+        assertThat(members.get(2).getUsername()).isNull();
+
+    }
+
+    @Test
+    void paging() {
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(results.getTotal()).isEqualTo(4);
+    }
+
+    @Test
+    void aggregation() {
+        List<Tuple> result = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+
+    }
+
 }
